e4669977fcf919fbe23d1fde19fcd099cfc6671b
MasrukulMridul <masrukulmridul15@gmail.com>
2021-01-22 03:31:32 +0600

    final code

--- megaFriend.js
+++ megaFriend.js
@@ -1,6 +1,6 @@
-function megaFriend()
+function megaFriend("Munna","Zafree","Elon Musks","Masrukul")
 {
-var friendsName = ["Munna","Zafree","Elon Musk","Masrukul"];
+var friendsName = ["Munna","Zafree","Elon Musks","Masrukul"];
 
 var highest = friendsName[0];
 for(var i = 0; i< friendsName.length;i++)
